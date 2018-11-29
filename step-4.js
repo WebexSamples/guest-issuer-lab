da6b3f657b3151021762ae635801b2f109680c05
Adam Kalsey <adam@kalsey.com>
2018-11-28 19:37:07 -0800

    Add comments and email config

--- step-4.js
+++ step-4.js
@@ -5,7 +5,8 @@ var config = {
   "userUniqueId": "0xDECAFBAD", // A unique ID for your user
   "userName": "Michael Dolly", // Display name, pick anything you'd like.
   "issuerId": "YOUR-ISSUER-ID-HERE", // Use your Guest Issuer ID from your Guest app
-  "issuerSecret":  "YOUR-ISSUER-SECRET" // Use the Guest Issuer Shared Secret from your Guest app
+  "issuerSecret":  "YOUR-ISSUER-SECRET", // Use the Guest Issuer Shared Secret from your Guest app
+  "yourEmail":  "you@example.com" // The email address you use with Webex Teams
 };
 
 jwt.sign(
@@ -14,14 +15,15 @@ jwt.sign(
     "name": config.userName,
     "iss": config.issuerId
   },
-  Buffer.from(config.issuerSecret, 'base64'), // The shared secret from your Guest app
+  Buffer.from(config.issuerSecret, 'base64'), 
   { expiresIn: '10m' },
   function(err, token) {
     console.log({token});
     const webex = new CiscoWebex();
+    // Use the JWT to authenticate the webex object
     webex.authorization.requestAccessTokenFromJwt({"jwt":token}).then(() => {
         if (webex.canAuthorize) {
-            // Authorization is successful
+            // Authorization is successful, send a message
             return webex.messages.create({
                 text: 'Howdy!',
                 toPersonEmail: config.yourEmail
